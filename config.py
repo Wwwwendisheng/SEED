a631e08a5972ed0f52365a83bffd333f632def00
Wendisheng <87435580+Wwwwendisheng@users.noreply.github.com>
2024-01-20 04:48:47 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 import sys
-sys.path.append('./')
+sys.path.append('./SEED/')
 
 import six
 import os
